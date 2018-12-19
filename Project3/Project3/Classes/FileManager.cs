780fbfbbde1616c895dfe727032ac38f1623eda9
Antoshchuk Nadiya <n.antoshuk52@gmail.com>
2018-12-19 17:22:41 +0200

    Create project

---
+++ Project3/Project3/Classes/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Project3.Classes
+{
+    /// <summary>
+    /// Interface FileManager for control some file elems and had some value
+    /// </summary>
+    interface IFileManager
+    {
+        /// <summary>
+        /// Method witch read file
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Read(string filePath);
+        /// <summary>
+        /// Method witch write to file
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Write(string filePath);
+    }
+
+}
