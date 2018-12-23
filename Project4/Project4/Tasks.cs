3d3539dda4209d050cf8280fbf18d6d0ea1f2a59
Antoshchuk Nadiya <n.antoshuk52@gmail.com>
2018-12-23 16:05:32 +0200

    Conect database

--- Project4/Project4/Tasks.cs
+++ Project4/Project4/Tasks.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Project4
 {
     class Tasks
     {
+        /// <summary>
+        /// Connection string to Northwind database
+        /// </summary>
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["NORTHWNDConnectionString"].ConnectionString;
+
+        /// <summary>
+        /// Represents a unique session to a SQL Server data source.
+        /// </summary>
+        private SqlConnection connection;
+
+        /// <summary>
+        /// Allows to query and send commands to a database
+        /// </summary>
+        private SqlCommand command;
+
+        /// <summary>
+        /// Is used to read data from a data source
+        /// </summary>
+        private SqlDataReader reader;
     }
 }
